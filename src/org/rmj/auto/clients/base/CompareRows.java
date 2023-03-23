74bb25dd599251fc1d7408d353893aa064bc628c
jarcilla20 <jahnarcilla@yahoo.com>
2023-03-23 08:55:35 +0800

    added condition
    added condition return false if rows are null

--- src/org/rmj/auto/clients/base/CompareRows.java
+++ src/org/rmj/auto/clients/base/CompareRows.java
@@ -15,17 +15,21 @@ import javax.sql.rowset.CachedRowSet;
  */
 public class CompareRows {    
     public static boolean isRowEqual(CachedRowSet row1, CachedRowSet row2) throws SQLException{
-        row1.beforeFirst();
-        row2.beforeFirst();
-        while (row1.next() && row2.next()) {
-            int columnCount = row1.getMetaData().getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                Object value1 = row1.getObject(i);
-                Object value2 = row2.getObject(i);
-                if (!Objects.equals(value1, value2)) {
-                    return false;
+        if(row1 != null && row2 != null){
+            row1.beforeFirst();
+            row2.beforeFirst();
+            while (row1.next() && row2.next()) {
+                int columnCount = row1.getMetaData().getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    Object value1 = row1.getObject(i);
+                    Object value2 = row2.getObject(i);
+                    if (!Objects.equals(value1, value2)) {
+                        return false;
+                    }
                 }
             }
+        }else{
+            return false;
         }
     return true;          
     }
